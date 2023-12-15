4afd59ff4f87bd335dd5edc711be6453cdeb1e93
rahatbhambri97 <rahat.bhambri@alpha-grep.com>
2023-12-15 13:48:25 +0530

    game made

--- src/App.vue
+++ src/App.vue
@@ -3,42 +3,43 @@
 
 
 <p style="font-size: xx-large ; font-style: initial; color: rgb(24, 7, 93);"> <strong> Wack a mole! </strong> </p>
-<button @click = "startGame()"> Start Game </button>
-<button @click = "endGame()"> End Game</button>
+<button @click = "startGame()" v-if="!start"> Start Game </button>
+<button @click = "endGame()" v-if="!end" style="background-color: red;"> End Game</button>
 
   <div class = "score" v-if="start">
-    <p> {{  score }}</p>
+    <strong><p> {{  score }}</p></strong>
   </div>
 
   <div class = "game" v-if=start>
     
       
-      <div class = "hole hole1"> 
-        <div class = "mole mole1" id ="mole1">
+      <div class = "hole hole1" @click="whack($event)"> 
+        <div class = "mole mole1" id ="mole1" >
         </div>
       </div>
-      <div class = "hole hole2"> 
+      <div class = "hole hole2" @click="whack($event)"> 
         <div class = "mole" id ="mole2">
         </div>
       </div>
-      <div class = "hole hole3"> 
+      <div class = "hole hole3" @click="whack($event)"> 
         <div class = "mole" id ="mole3">
         </div>
       </div>
-      <div class = "hole hole4"> 
+      <div class = "hole hole4" @click="whack($event)"> 
         <div class = "mole" id ="mole4">
         </div>
       </div>
-      <div class = "hole hole5"> 
+      <div class = "hole hole5" @click="whack($event)"> 
         <div class = "mole" id ="mole5">
         </div>
       </div>
-      <div class = "hole hole6"> 
+      <div class = "hole hole6" @click="whack($event)"> 
         <div class = "mole" id ="mole6">
         </div>
       </div>
-      
   </div>
+  <br>
+  <br>
 
   
   <div class = "endscreen" v-if=isEnded>
@@ -72,9 +73,15 @@ function getRandomInteger(min, max) {
         this.end = false;
         this.start = true;
         this.score = 0 ;
+        setTimeout(()=>{
+          this.end = true ;
+          this.start = false;
+        }, 15000) ;
+
         while(!this.end){
           await this.toggleMole();
         }
+        
 
       }, 
       incScore(){
@@ -95,8 +102,24 @@ function getRandomInteger(min, max) {
           await new Promise(resolve => setTimeout(resolve, 1000) ) ;
           sty.toggle('up') ;
         }
+      },
+
+      async whack(event){
+        const hole = event.target ;
+        const mole = hole.querySelector('.mole');
         
-      }
+        if (hole.classList.contains('up')){
+          this.incScore() ;
+          console.log("whacked") ;
+          mole.classList.add('wacked');
+          await new Promise(resolve => setTimeout(resolve, 500) ) ;
+          hole.classList.remove('up'); //this refers to item clicked
+          mole.classList.remove("wacked") ;
+
+        }
+        
+      }     
+  
     }
   })
 </script>
@@ -128,28 +151,36 @@ h1 {
     width: 10%;
     line-height: 1;
     border-radius: 16px;
-    color: #fff;
-    margin-left: 340px;
+    color: #ffffff;
+    /* margin-left: 340px; */
+    margin: 0 auto; /* Horizontally center */
+    border-color: rgb(255, 255, 255);
+    outline: solid 5px rgb(212, 255, 0);
 }
 
 .score {
-    background:rgb(189, 9, 9);
+    background:rgb(156, 189, 9);
     padding: 0 48px;
     display: flex;
     width: 10%;
-    line-height: 1;
+    line-height: 2;
     border-radius: 16px;
-    color: #fff;
-    margin-left: 340px;
+    color: #000000;
+    margin: 0 auto;
+    margin-top: 5%;
+    font-size: x-large;
+
 }
 
 .game {
     width: 800px;
-    height: 1400px;
+    height: 500px;
     display:flex;
     flex-wrap:wrap;
     margin: 0 auto;
     background-color: aquamarine;
+    border-color: black;
+    outline: solid 5px black;
     
     
 }
@@ -166,36 +197,39 @@ h1 {
     background: url(dirt.png) bottom center no-repeat;
     background-size:contain;
     content: '';
-    width: 140%;
-    height: 60%;
+    width: 100%;
+    height: 100px;
     position: absolute;
     z-index: 2;
+    padding-top: 50%;
     /* bottom: -30px; */
 }
 
 .mole {
     background: url('mole.png') bottom center no-repeat;
-    background-size: 40%;
+    background-size: 50%;
     position: absolute;
     top: 35%;
-    width: 100%;
-    height: 90px;
+    width: 60%;
+    height: 70px;
     transition: all 0.4s;
     padding: 10%;
+    padding-left: 20%;
 }
 
 .mole.wacked {
     background: url('mole_wacked.png') bottom center no-repeat;
-    background-size: 80%;
+    background-size: 50%;
     position: absolute;
-    top: 100%;
-    width: 100%;
-    height: 100%;
+    top: 35%;
+    width: 60%;
+    height: 70px;
     transition: all 0.4s;
+    padding: 10%;
 }
 
 .hole.up .mole{
-    top:28%;
+    top:25%;
 }
 
 button{
@@ -205,7 +239,9 @@ button{
     color: #fff;
     background-color: blue;
     border-radius: 10px;
-    margin-left: 190px;
+    margin: 0 auto;
     margin-top: 90px;
+    border-color: rgb(255, 255, 255);
+    outline: solid 5px black;
 }
 </style>
